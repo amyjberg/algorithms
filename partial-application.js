1bdc11f5e18b8af710adae7c35111a93149a5609
Amy Berg <aberg2014@gmail.com>
2018-09-11 21:28:16 -0400

    partial application with comments

---
+++ partial-application.js
@@ -0,0 +1,15 @@
+
+function partialApplication(func) {
+  // partial application returns a new version of the func
+  return function(...args) {
+    // we have access to func.length due to closure
+    if (args.length === func.length) {
+      // we have all the arguments we need to execute, so we will execute
+      return func(...args)
+    } else {
+      // we have some, but not all the arguments we need
+      // the bind method returns a new function with reduced arity
+      return partialApplication(func.bind(null, ...args))
+    }
+  }
+}
