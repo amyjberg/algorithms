8ac369674bd99fe624005f234775d9bc32a17a4a
Amy Berg <aberg2014@gmail.com>
2018-09-13 12:52:04 -0400

    added iterative solution for permutations in python

--- python/permutations.py
+++ python/permutations.py
@@ -1,3 +1,21 @@
+def getPermutations(string):
+  letters = list(string)
+  results = [[letters.pop(0)]] # start with one array with one letter in results array
+  while len(letters) > 0:
+    currentLetter = letters.pop(0) # decreases length of letters by one each pass through the loop
+    temporaryResults = []
+    for x in xrange(0, len(results)):
+      for y in xrange(0, len(results[x]) + 1):
+        resultCopy = results[x][:] # using slicing to get copy
+        resultCopy.insert(y, currentLetter)
+        temporaryResults.append(resultCopy)
+    results = temporaryResults
+  permutations = []
+  for z in xrange(0, len(results)):
+    permutations.append(''.join(results[z]))
+  print(permutations)
+  return permutations
+
 def getPermutationsRec(string):
   permutations = []
   if len(string) == 1:
